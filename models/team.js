aa1c664c57e959f14adc591dd87a61e8322ca7a2
rhysbrame <25747279+rhysbrame@users.noreply.github.com>
2021-05-11 17:22:20 +0100

    Created schema for team

---
+++ models/team.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const teamSchema = new mongoose.Schema({
+    teamId: {
+        type: Number,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    }
+})
+
+const Team = mongoose.model('Team', teamSchema)
+
+module.exports = Team;
