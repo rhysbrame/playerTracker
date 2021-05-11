cdfa78c72484679b00a6aa223205e4f54c7f84f2
rhysbrame <25747279+rhysbrame@users.noreply.github.com>
2021-05-11 17:15:35 +0100

    Changed the file structure for the seeding process

---
+++ seeds/ReadMe.md
@@ -0,0 +1,7 @@
+Directions for seeding the database:
+
+change directory to playerTracker.
+run the command:
+'node seeds/index.js'
+
+check Mongo for changes
---
+++ seeds/playerSeeds.js
@@ -0,0 +1,72 @@
+module.exports.playerSeeds = [
+    {
+        playerId: 20875,
+        firstname: 'DK',
+        lastname: 'Metcalf',
+        number: 14,
+        position: 'WR',
+        team: 'SEA',
+        teamID: 30,
+        photoUrl: 'https://s3-us-west-2.amazonaws.com/static.fantasydata.com/headshots/nfl/low-res/20875.png'
+    },
+    {
+        playerId: 14536,
+        firstname: 'Russell',
+        lastname: 'Wilson',
+        number: 3,
+        position: 'QB',
+        team: 'SEA',
+        teamID: 30,
+        photoUrl: 'https://s3-us-west-2.amazonaws.com/static.fantasydata.com/headshots/nfl/low-res/14536.png'
+    },
+    {
+        playerId: 16830,
+        firstname: 'Tyler',
+        lastname: 'Lockett',
+        number: 16,
+        position: 'WR',
+        team: 'SEA',
+        teamID: 30,
+        photoUrl: 'https://s3-us-west-2.amazonaws.com/static.fantasydata.com/headshots/nfl/low-res/16830.png'
+    },
+    {
+        playerId: 18923,
+        firstname: 'Jamal',
+        lastname: 'Adams',
+        number: 33,
+        position: 'SS',
+        team: 'SEA',
+        teamID: 30,
+        photoUrl: 'https://s3-us-west-2.amazonaws.com/static.fantasydata.com/headshots/nfl/low-res/16830.png'
+    },
+    {
+        playerId: 14534,
+        firstname: 'Bobby',
+        lastname: 'Wagner',
+        number: 54,
+        position: 'ILB',
+        team: 'SEA',
+        teamID: 30,
+        photoUrl: 'https://s3-us-west-2.amazonaws.com/static.fantasydata.com/headshots/nfl/low-res/14534.png'
+    },
+    {
+        playerId: 14531,
+        firstname: 'Rhys',
+        lastname: 'Brame',
+        number: 14,
+        position: 'CB',
+        team: 'SEA',
+        teamID: 30,
+        photoUrl: 'https://s3-us-west-2.amazonaws.com/static.fantasydata.com/headshots/nfl/low-res/14534.png'
+    },
+    {
+        playerId: 14931,
+        firstname: 'Graham',
+        lastname: 'Sayer',
+        number: 14,
+        position: 'CB',
+        team: 'SEA',
+        teamID: 30,
+        photoUrl: 'https://s3-us-west-2.amazonaws.com/static.fantasydata.com/headshots/nfl/low-res/14534.png'
+    }
+]
