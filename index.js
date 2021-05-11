9894102fbfff7d04d28713ef068aa3b8895dc9c0
rhysbrame <25747279+rhysbrame@users.noreply.github.com>
2021-05-11 13:38:57 +0100

    Updated the mongoose connection and error handling

--- index.js
+++ index.js
@@ -5,14 +5,16 @@ const mongoose = require('mongoose');
 
 const Player = require('./models/player');
 
-mongoose.connect('mongodb://localhost:27017/playerTracker', { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => {
-        console.log("Mongoose CONNECTION OPEN!")
-    })
-    .catch(err => {
-        console.log("Mongoose ERROR!")
-        console.log(err)
-    })
+mongoose.connect('mongodb://localhost:27017/playerTracker', { 
+    useNewUrlParser: true, 
+    useUnifiedTopology: true 
+})
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database connected");
+});
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs')
