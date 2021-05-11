e2fac869e0a3c10e6f6b7414c56f1bf78562c3f6
rhysbrame <25747279+rhysbrame@users.noreply.github.com>
2021-05-11 14:22:55 +0100

    Added a home route and navigation between routes

--- index.js
+++ index.js
@@ -19,6 +19,10 @@ db.once("open", () => {
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs')
 
+app.get('/', (req, res) => {
+    res.render('home');
+})
+
 app.get('/players', async (req, res) => {
     const players = await Player.find({});
     res.render('players/index', {players})
