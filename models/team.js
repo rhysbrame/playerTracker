a769c24a9d4d745c7db51cce9ec016699dc54996
rhysbrame <25747279+rhysbrame@users.noreply.github.com>
2021-05-15 09:24:54 +0100

    Changed the schema for player

--- models/team.js
+++ models/team.js
@@ -1,24 +1,18 @@
 const mongoose = require('mongoose');
 
-const teamSchema = new mongoose.Schema({
-    teamId: {
-        type: Number,
-        required: true
-    },
-    name: {
-        type: String,
-        required: true
-    },
-    stadiumName: {
-        type: String,
-        required: true
-    },
-    logoUrl: {
-        type: String,
-        required: true
-    }
+const collegeTeamSchema = new mongoose.Schema({
+    TeamID: { type: Number, required: true },
+    Key: { type: String, required: true },
+    School: { type: String, required: true },
+    Name: { type: String, required: true },
+    StadiumID: { type: Number, required: true },
+    GlobalTeamID: { type: Number, required: true },
+    TeamLogoUrl: { type: String, required: true },
+    ConferenceID: { type: Number },
+    Conference: { type: String },
+    ShortDisplayName: { type: String, required: true }
 })
 
-const Team = mongoose.model('Team', teamSchema)
+const Team = mongoose.model('Team', collegeTeamSchema)
 
 module.exports = Team;
