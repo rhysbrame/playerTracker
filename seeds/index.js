b6b26cae916f267c62165c3df0708a145682c8c0
rhysbrame <25747279+rhysbrame@users.noreply.github.com>
2021-05-20 20:15:17 +0100

    Changed console message for swatch counting

--- seeds/index.js
+++ seeds/index.js
@@ -25,7 +25,7 @@ const masterSeedDB = async () => {
     counter++;
     const t = new Team(team);
     const swatches = await Vibrant.from(`${team.TeamLogoUrl}`).getSwatches();
-    console.log('***swatches***', counter);
+    console.log('Number of team swatches from 252', counter);
     t.Swatches = swatches;
     await t.save();
   }
