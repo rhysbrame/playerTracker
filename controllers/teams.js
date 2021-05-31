c9ab921d5eba626da823dc99485228973ce3d438
rhysbrame <25747279+rhysbrame@users.noreply.github.com>
2021-05-31 14:40:40 +0100

    Refactor with controllers

---
+++ controllers/teams.js
@@ -0,0 +1,21 @@
+const Team = require('../models/team');
+const ObjectID = require('mongodb').ObjectID;
+
+module.exports.index = async (req, res) => {
+  const teams = await Team.find({});
+  res.render('teams/index', { teams });
+};
+
+module.exports.details = async (req, res, next) => {
+  const { id } = req.params;
+  if (!ObjectID.isValid(id)) {
+    req.flash('error', 'Could not find that team, Invalid ID!');
+    return res.redirect('/teams/');
+  }
+  const team = await Team.findById(id).populate('RosterPlayerIDs');
+  if (!team) {
+    req.flash('error', 'Could not find that team!');
+    return res.redirect('/teams/');
+  }
+  res.render('teams/details', { team });
+};
--- routes/teams.js
+++ routes/teams.js
@@ -1,35 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const ObjectID = require('mongodb').ObjectID;
 
-const { isLoggedIn } = require('../utilities/middleware');
+const teams = require('../controllers/teams');
 const catchAsyncWrapper = require('../utilities/catchAsyncWrapper.js');
-const Team = require('../models/team');
+const { isLoggedIn } = require('../utilities/middleware');
 
-router.get(
-  '/',
-  catchAsyncWrapper(async (req, res) => {
-    const teams = await Team.find({});
-    res.render('teams/index', { teams });
-  })
-);
+router.get('/', catchAsyncWrapper(teams.index));
 
-router.get(
-  '/:id',
-  isLoggedIn,
-  catchAsyncWrapper(async (req, res, next) => {
-    const { id } = req.params;
-    if (!ObjectID.isValid(id)) {
-      req.flash('error', 'Could not find that team, Invalid ID!');
-      return res.redirect('/teams/');
-    }
-    const team = await Team.findById(id).populate('RosterPlayerIDs');
-    if (!team) {
-      req.flash('error', 'Could not find that team!');
-      return res.redirect('/teams/');
-    }
-    res.render('teams/details', { team });
-  })
-);
+router.get('/:id', isLoggedIn, catchAsyncWrapper(teams.details));
 
 module.exports = router;
