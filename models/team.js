e7e68103a285df9feba24f458142c116e68156ca
rhysbrame <25747279+rhysbrame@users.noreply.github.com>
2021-06-05 09:34:00 +0100

    Mapbox: Added dynamic pop up text to map

--- models/team.js
+++ models/team.js
@@ -1,58 +1,63 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
-const collegeTeamSchema = new Schema({
-  TeamID: { type: Number, required: true },
-  Key: { type: String, required: true },
-  School: { type: String, required: true },
-  Name: { type: String, required: true },
-  StadiumID: { type: Number, required: true },
-  GlobalTeamID: { type: Number, required: true },
-  TeamLogoUrl: { type: String, required: true },
-  ConferenceID: { type: Number },
-  Conference: { type: String },
-  ShortDisplayName: { type: String, required: true },
-  UserFavourited: {
-    type: Boolean,
-    required: true,
-    default: false,
-  },
-  RosterPlayerIDs: [{ type: Schema.Types.ObjectId, ref: 'Player' }],
-  Swatches: {
-    Vibrant: {
-      _rgb: [{ type: Number }, { type: Number }, { type: Number }],
-    },
-    DarkVibrant: {
-      _rgb: [{ type: Number }, { type: Number }, { type: Number }],
-    },
-    LightVibrant: {
-      _rgb: [{ type: Number }, { type: Number }, { type: Number }],
-    },
-    Muted: {
-      _rgb: [{ type: Number }, { type: Number }, { type: Number }],
-    },
-    DarkMuted: {
-      _rgb: [{ type: Number }, { type: Number }, { type: Number }],
-    },
-    LightMuted: {
-      _rgb: [{ type: Number }, { type: Number }, { type: Number }],
-    },
-  },
-  StadiumData: { type: Schema.Types.ObjectId, ref: 'Stadium' },
-  type: { type: String, default: 'Feature' },
-  geometry: {
-    type: {
-      type: String,
-      enum: ['Point'],
+const opts = { toJSON: { virtuals: true } };
+
+const collegeTeamSchema = new Schema(
+  {
+    TeamID: { type: Number, required: true },
+    Key: { type: String, required: true },
+    School: { type: String, required: true },
+    Name: { type: String, required: true },
+    StadiumID: { type: Number, required: true },
+    GlobalTeamID: { type: Number, required: true },
+    TeamLogoUrl: { type: String, required: true },
+    ConferenceID: { type: Number },
+    Conference: { type: String },
+    ShortDisplayName: { type: String, required: true },
+    UserFavourited: {
+      type: Boolean,
       required: true,
-      default: 'Point',
+      default: false,
     },
-    coordinates: {
-      type: [Number],
-      required: true,
+    RosterPlayerIDs: [{ type: Schema.Types.ObjectId, ref: 'Player' }],
+    Swatches: {
+      Vibrant: {
+        _rgb: [{ type: Number }, { type: Number }, { type: Number }],
+      },
+      DarkVibrant: {
+        _rgb: [{ type: Number }, { type: Number }, { type: Number }],
+      },
+      LightVibrant: {
+        _rgb: [{ type: Number }, { type: Number }, { type: Number }],
+      },
+      Muted: {
+        _rgb: [{ type: Number }, { type: Number }, { type: Number }],
+      },
+      DarkMuted: {
+        _rgb: [{ type: Number }, { type: Number }, { type: Number }],
+      },
+      LightMuted: {
+        _rgb: [{ type: Number }, { type: Number }, { type: Number }],
+      },
+    },
+    StadiumData: { type: Schema.Types.ObjectId, ref: 'Stadium' },
+    type: { type: String, default: 'Feature' },
+    geometry: {
+      type: {
+        type: String,
+        enum: ['Point'],
+        required: true,
+        default: 'Point',
+      },
+      coordinates: {
+        type: [Number],
+        required: true,
+      },
     },
   },
-});
+  opts
+);
 
 collegeTeamSchema.virtual('Coordinates').get(function () {
   const geometryObject = {
@@ -60,6 +65,14 @@ collegeTeamSchema.virtual('Coordinates').get(function () {
     coordinates: [this.StadiumData.GeoLong, this.StadiumData.GeoLat],
   };
   return geometryObject;
+});
+
+collegeTeamSchema.virtual('properties.popUpMarkup').get(function () {
+  return `
+  <strong>
+    <a href="/teams/${this._id}">${this.School} ${this.Name}</a>
+  <strong>
+  `;
 });
 
 module.exports = mongoose.model('Team', collegeTeamSchema);
--- public/js/clusterMap.js
+++ public/js/clusterMap.js
@@ -1,5 +1,5 @@
 mapboxgl.accessToken = mapToken;
-var map = new mapboxgl.Map({
+const map = new mapboxgl.Map({
   container: 'map',
   style: 'mapbox://styles/mapbox/light-v10',
   center: [-103.59179687498357, 40.66995747013945],
@@ -63,7 +63,7 @@ map.on('load', function () {
     filter: ['!', ['has', 'point_count']],
     paint: {
       'circle-color': '#11b4da',
-      'circle-radius': 4,
+      'circle-radius': 7,
       'circle-stroke-width': 1,
       'circle-stroke-color': '#fff',
     },
@@ -71,10 +71,10 @@ map.on('load', function () {
 
   // inspect a cluster on click
   map.on('click', 'clusters', function (e) {
-    var features = map.queryRenderedFeatures(e.point, {
+    const features = map.queryRenderedFeatures(e.point, {
       layers: ['clusters'],
     });
-    var clusterId = features[0].properties.cluster_id;
+    const clusterId = features[0].properties.cluster_id;
     map
       .getSource('teams')
       .getClusterExpansionZoom(clusterId, function (err, zoom) {
@@ -92,15 +92,9 @@ map.on('load', function () {
   // the location of the feature, with
   // description HTML from its properties.
   map.on('click', 'unclustered-point', function (e) {
-    var coordinates = e.features[0].geometry.coordinates.slice();
-    var mag = e.features[0].properties.mag;
-    var tsunami;
-
-    if (e.features[0].properties.tsunami === 1) {
-      tsunami = 'yes';
-    } else {
-      tsunami = 'no';
-    }
+    const text = e.features[0].properties.popUpMarkup;
+    const coordinates = e.features[0].geometry.coordinates.slice();
+    const mag = e.features[0].properties.mag;
 
     // Ensure that if the map is zoomed out such that
     // multiple copies of the feature are visible, the
@@ -109,16 +103,19 @@ map.on('load', function () {
       coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
     }
 
-    new mapboxgl.Popup()
-      .setLngLat(coordinates)
-      .setHTML('magnitude: ' + mag + '<br>Was there a tsunami?: ' + tsunami)
-      .addTo(map);
+    new mapboxgl.Popup().setLngLat(coordinates).setHTML(text).addTo(map);
   });
 
   map.on('mouseenter', 'clusters', function () {
     map.getCanvas().style.cursor = 'pointer';
   });
   map.on('mouseleave', 'clusters', function () {
+    map.getCanvas().style.cursor = '';
+  });
+  map.on('mouseenter', 'unclustered-point', function () {
+    map.getCanvas().style.cursor = 'pointer';
+  });
+  map.on('mouseleave', 'unclustered-point', function () {
     map.getCanvas().style.cursor = '';
   });
 });
