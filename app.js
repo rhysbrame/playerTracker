37822fa3ca02590eaec8bd09cb9e50c9a6253e4a
rhysbrame <25747279+rhysbrame@users.noreply.github.com>
2021-06-17 14:14:41 +0100

    Added npm start script and changed port for Heroku

--- app.js
+++ app.js
@@ -117,6 +117,7 @@ app.use((err, req, res, next) => {
   res.status(statusCode).render('error', { err });
 });
 
-app.listen(3000, () => {
-  console.log('App is listening on port 3000');
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`App is listening on port ${port}`);
 });
